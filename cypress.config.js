4a3358553370fc2206f9a6e2ee50ea7ee0f7579b
Apeksha Bhosale <7846888+ApekshaBhosale@users.noreply.github.com>
2024-04-16 20:15:55 +0530

    small change

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   integrationFolder: 'cypress/integration',
-  testFiles: "**/*.spec.js", // Replace 'your_spec_file.spec.js' with your actual spec file
+  testFiles: "**/*_spec.js", // Replace 'your_spec_file.spec.js' with your actual spec file
+  video: false, // Disable video recording to save resources
+  screenshotsFolder: false, // Disable screenshots to save resources
+}
+
+module.exports = {
+  integrationFolder: 'cypress/integration',
+  testFiles: '**/*.spec.js', // Change this to match the pattern of your spec files
   video: false, // Disable video recording to save resources
   screenshotsFolder: false, // Disable screenshots to save resources
 }
